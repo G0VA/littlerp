e5bcfbe621e6ded214d7eb30b76eac4f1bf19ff1
G0VA <44608163+G0VA@users.noreply.github.com>
2018-11-26 14:14:21 +0100

    fase01
    inicio

---
+++ Avengers/Avengers/Presentacion/Menu.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Avengers.Presentacion
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.products = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.customers = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.users = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnOrders = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnExit = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnSys = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.panel1.SuspendLayout();
+            this.bunifuGradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bunifuGradientPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(177, 372);
+            this.panel1.TabIndex = 0;
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.products);
+            this.bunifuGradientPanel1.Controls.Add(this.customers);
+            this.bunifuGradientPanel1.Controls.Add(this.users);
+            this.bunifuGradientPanel1.Controls.Add(this.btnOrders);
+            this.bunifuGradientPanel1.Controls.Add(this.btnExit);
+            this.bunifuGradientPanel1.Controls.Add(this.btnSys);
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.MediumBlue;
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.Blue;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.Gray;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.White;
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(177, 372);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // products
+            // 
+            this.products.Activecolor = System.Drawing.Color.Transparent;
+            this.products.BackColor = System.Drawing.Color.Transparent;
+            this.products.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.products.BorderRadius = 0;
+            this.products.ButtonText = "       Products";
+            this.products.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.products.DisabledColor = System.Drawing.Color.Gray;
+            this.products.Enabled = false;
+            this.products.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.products.Iconcolor = System.Drawing.Color.Transparent;
+            this.products.Iconimage = ((System.Drawing.Image)(resources.GetObject("products.Iconimage")));
+            this.products.Iconimage_right = null;
+            this.products.Iconimage_right_Selected = null;
+            this.products.Iconimage_Selected = null;
+            this.products.IconMarginLeft = 0;
+            this.products.IconMarginRight = 0;
+            this.products.IconRightVisible = true;
+            this.products.IconRightZoom = 0D;
+            this.products.IconVisible = false;
+            this.products.IconZoom = 90D;
+            this.products.IsTab = false;
+            this.products.Location = new System.Drawing.Point(0, 120);
+            this.products.Name = "products";
+            this.products.Normalcolor = System.Drawing.Color.Transparent;
+            this.products.OnHovercolor = System.Drawing.Color.DodgerBlue;
+            this.products.OnHoverTextColor = System.Drawing.Color.White;
+            this.products.selected = false;
+            this.products.Size = new System.Drawing.Size(177, 19);
+            this.products.TabIndex = 5;
+            this.products.Text = "       Products";
+            this.products.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.products.Textcolor = System.Drawing.Color.White;
+            this.products.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.products.Visible = false;
+            // 
+            // customers
+            // 
+            this.customers.Activecolor = System.Drawing.Color.Transparent;
+            this.customers.BackColor = System.Drawing.Color.Transparent;
+            this.customers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.customers.BorderRadius = 0;
+            this.customers.ButtonText = "       Customers";
+            this.customers.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.customers.DisabledColor = System.Drawing.Color.Gray;
+            this.customers.Enabled = false;
+            this.customers.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.customers.Iconcolor = System.Drawing.Color.Transparent;
+            this.customers.Iconimage = ((System.Drawing.Image)(resources.GetObject("customers.Iconimage")));
+            this.customers.Iconimage_right = null;
+            this.customers.Iconimage_right_Selected = null;
+            this.customers.Iconimage_Selected = null;
+            this.customers.IconMarginLeft = 0;
+            this.customers.IconMarginRight = 0;
+            this.customers.IconRightVisible = true;
+            this.customers.IconRightZoom = 0D;
+            this.customers.IconVisible = false;
+            this.customers.IconZoom = 90D;
+            this.customers.IsTab = false;
+            this.customers.Location = new System.Drawing.Point(0, 95);
+            this.customers.Name = "customers";
+            this.customers.Normalcolor = System.Drawing.Color.Transparent;
+            this.customers.OnHovercolor = System.Drawing.Color.DodgerBlue;
+            this.customers.OnHoverTextColor = System.Drawing.Color.White;
+            this.customers.selected = false;
+            this.customers.Size = new System.Drawing.Size(177, 19);
+            this.customers.TabIndex = 4;
+            this.customers.Text = "       Customers";
+            this.customers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.customers.Textcolor = System.Drawing.Color.White;
+            this.customers.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customers.Visible = false;
+            // 
+            // users
+            // 
+            this.users.Activecolor = System.Drawing.Color.Transparent;
+            this.users.BackColor = System.Drawing.Color.Transparent;
+            this.users.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.users.BorderRadius = 0;
+            this.users.ButtonText = "       Users";
+            this.users.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.users.DisabledColor = System.Drawing.Color.Gray;
+            this.users.Enabled = false;
+            this.users.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.users.Iconcolor = System.Drawing.Color.Transparent;
+            this.users.Iconimage = ((System.Drawing.Image)(resources.GetObject("users.Iconimage")));
+            this.users.Iconimage_right = null;
+            this.users.Iconimage_right_Selected = null;
+            this.users.Iconimage_Selected = null;
+            this.users.IconMarginLeft = 0;
+            this.users.IconMarginRight = 0;
+            this.users.IconRightVisible = true;
+            this.users.IconRightZoom = 0D;
+            this.users.IconVisible = false;
+            this.users.IconZoom = 90D;
+            this.users.IsTab = false;
+            this.users.Location = new System.Drawing.Point(0, 70);
+            this.users.Name = "users";
+            this.users.Normalcolor = System.Drawing.Color.Transparent;
+            this.users.OnHovercolor = System.Drawing.Color.DodgerBlue;
+            this.users.OnHoverTextColor = System.Drawing.Color.White;
+            this.users.selected = false;
+            this.users.Size = new System.Drawing.Size(177, 19);
+            this.users.TabIndex = 3;
+            this.users.Text = "       Users";
+            this.users.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.users.Textcolor = System.Drawing.Color.White;
+            this.users.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.users.Visible = false;
+            this.users.Click += new System.EventHandler(this.users_Click);
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Activecolor = System.Drawing.Color.Transparent;
+            this.btnOrders.BackColor = System.Drawing.Color.Transparent;
+            this.btnOrders.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnOrders.BorderRadius = 0;
+            this.btnOrders.ButtonText = "Orders";
+            this.btnOrders.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOrders.DisabledColor = System.Drawing.Color.Gray;
+            this.btnOrders.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnOrders.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnOrders.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnOrders.Iconimage")));
+            this.btnOrders.Iconimage_right = null;
+            this.btnOrders.Iconimage_right_Selected = null;
+            this.btnOrders.Iconimage_Selected = null;
+            this.btnOrders.IconMarginLeft = 0;
+            this.btnOrders.IconMarginRight = 0;
+            this.btnOrders.IconRightVisible = true;
+            this.btnOrders.IconRightZoom = 0D;
+            this.btnOrders.IconVisible = false;
+            this.btnOrders.IconZoom = 90D;
+            this.btnOrders.IsTab = false;
+            this.btnOrders.Location = new System.Drawing.Point(0, 60);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Normalcolor = System.Drawing.Color.Transparent;
+            this.btnOrders.OnHovercolor = System.Drawing.Color.DodgerBlue;
+            this.btnOrders.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnOrders.selected = false;
+            this.btnOrders.Size = new System.Drawing.Size(206, 30);
+            this.btnOrders.TabIndex = 2;
+            this.btnOrders.Text = "Orders";
+            this.btnOrders.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrders.Textcolor = System.Drawing.Color.White;
+            this.btnOrders.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExit.BorderRadius = 0;
+            this.btnExit.ButtonText = "                Exit";
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.DisabledColor = System.Drawing.Color.Gray;
+            this.btnExit.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnExit.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnExit.Iconimage")));
+            this.btnExit.Iconimage_right = null;
+            this.btnExit.Iconimage_right_Selected = null;
+            this.btnExit.Iconimage_Selected = null;
+            this.btnExit.IconMarginLeft = 0;
+            this.btnExit.IconMarginRight = 0;
+            this.btnExit.IconRightVisible = true;
+            this.btnExit.IconRightZoom = 0D;
+            this.btnExit.IconVisible = false;
+            this.btnExit.IconZoom = 90D;
+            this.btnExit.IsTab = false;
+            this.btnExit.Location = new System.Drawing.Point(0, 294);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Normalcolor = System.Drawing.Color.Transparent;
+            this.btnExit.OnHovercolor = System.Drawing.Color.DodgerBlue;
+            this.btnExit.OnHoverTextColor = System.Drawing.Color.Transparent;
+            this.btnExit.selected = false;
+            this.btnExit.Size = new System.Drawing.Size(177, 48);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "                Exit";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Textcolor = System.Drawing.Color.White;
+            this.btnExit.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSys
+            // 
+            this.btnSys.Activecolor = System.Drawing.Color.Transparent;
+            this.btnSys.BackColor = System.Drawing.Color.Transparent;
+            this.btnSys.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSys.BorderRadius = 0;
+            this.btnSys.ButtonText = "System";
+            this.btnSys.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSys.DisabledColor = System.Drawing.Color.Gray;
+            this.btnSys.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSys.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnSys.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnSys.Iconimage")));
+            this.btnSys.Iconimage_right = null;
+            this.btnSys.Iconimage_right_Selected = null;
+            this.btnSys.Iconimage_Selected = null;
+            this.btnSys.IconMarginLeft = 0;
+            this.btnSys.IconMarginRight = 0;
+            this.btnSys.IconRightVisible = true;
+            this.btnSys.IconRightZoom = 0D;
+            this.btnSys.IconVisible = false;
+            this.btnSys.IconZoom = 90D;
+            this.btnSys.IsTab = false;
+            this.btnSys.Location = new System.Drawing.Point(0, 35);
+            this.btnSys.Name = "btnSys";
+            this.btnSys.Normalcolor = System.Drawing.Color.Transparent;
+            this.btnSys.OnHovercolor = System.Drawing.Color.DodgerBlue;
+            this.btnSys.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnSys.selected = false;
+            this.btnSys.Size = new System.Drawing.Size(177, 30);
+            this.btnSys.TabIndex = 0;
+            this.btnSys.Text = "System";
+            this.btnSys.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSys.Textcolor = System.Drawing.Color.White;
+            this.btnSys.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSys.Click += new System.EventHandler(this.btnSys_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 372);
+            this.Controls.Add(this.panel1);
+            this.IsMdiContainer = true;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel panel1;
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private Bunifu.Framework.UI.BunifuFlatButton btnSys;
+        private Bunifu.Framework.UI.BunifuFlatButton btnExit;
+        private Bunifu.Framework.UI.BunifuFlatButton users;
+        private Bunifu.Framework.UI.BunifuFlatButton btnOrders;
+        private Bunifu.Framework.UI.BunifuFlatButton products;
+        private Bunifu.Framework.UI.BunifuFlatButton customers;
+    }
+}
---
+++ Avengers/Avengers/Presentacion/Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Avengers.Presentacion
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+               
+        }
+
+        private void mostrarBtn()
+        {
+            users.Visible = true;
+            users.Enabled = true;
+            customers.Visible = true;
+            customers.Enabled = true;
+            products.Visible = true;
+            products.Enabled = true;
+
+            btnOrders.Location = new Point(0, 160);
+        }
+
+        private void ocultarBtn()
+        {
+            users.Visible = false;
+            users.Enabled = false;
+            customers.Visible = false;
+            customers.Enabled = false;
+            products.Visible = false;
+            products.Enabled = false;
+
+            btnOrders.Location = new Point(0, 60);
+        }
+
+        private void users_Click(object sender, EventArgs e)
+        {
+            Users u1 = new Users();
+            u1.MdiParent = this;
+            u1.WindowState = FormWindowState.Maximized;
+            u1.Show();
+
+        }
+
+        private void btnSys_Click(object sender, EventArgs e)
+        {
+            mostrarBtn();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOrders_Click(object sender, EventArgs e)
+        {
+            ocultarBtn();
+        }
+    }
+}
